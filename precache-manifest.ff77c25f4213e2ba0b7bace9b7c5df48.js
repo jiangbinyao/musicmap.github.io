281843ab9558a94967a94ecdcd4d8268f28edf8e
江滨耀 <1146800334@qq.com>
2020-08-09 01:24:00 +0800

    add web page

---
+++ precache-manifest.ff77c25f4213e2ba0b7bace9b7c5df48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52217d32888902bab48ba0124d55cb70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9dfa1def07e4fcb0acd",
+    "url": "/static/css/main.8922733e.chunk.css"
+  },
+  {
+    "revision": "494a334dadbe976cc856",
+    "url": "/static/js/2.69a1b76f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.69a1b76f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9dfa1def07e4fcb0acd",
+    "url": "/static/js/main.cfd96dc3.chunk.js"
+  },
+  {
+    "revision": "3d2a97cd82f343089455",
+    "url": "/static/js/runtime-main.238e75c2.js"
+  },
+  {
+    "revision": "b294a66012ed1ef7045407d58f31095b",
+    "url": "/static/media/yinji.b294a660.jpg"
+  }
+]);
